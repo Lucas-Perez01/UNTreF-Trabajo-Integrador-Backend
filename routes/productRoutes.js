b17a211458144044c4b4b018737735430efd5d15
Lucas <paragithub01@gmail.com>
2025-06-29 21:03:20 -0300

    Rutas/endpoints de los productos

---
+++ routes/productRoutes.js
@@ -0,0 +1,30 @@
+import express from "express";
+import {
+  getProductos,
+  productoPorCodigo,
+  crearProducto,
+  modificarProducto,
+  borrarProducto,
+} from "../controllers/productController.js";
+
+const router = express.Router();
+
+// Ruta para obtener todos los productos
+
+router.get("/productos", getProductos);
+
+// Ruta para obtener un producto por su código
+router.get("/productos/:codigo", productoPorCodigo);
+
+// Ruta para crear un nuevo producto
+
+router.post("/productos", crearProducto);
+
+// Ruta para modificar un producto por su código
+
+router.put("/productos/:codigo", modificarProducto);
+
+// Ruta para borrar un producto por su código
+router.delete("/productos/:codigo", borrarProducto);
+
+export default router;
